318c6b9cd63e72e2a034f03a8a311c59686f7211
MrCowDing <alexmebo17@gmail.com>
2022-07-29 19:31:18 +0800

    update

---
+++ 1. PRESENT.rb
@@ -0,0 +1,72 @@
+1. PRESENT
+
+ Simple present tense
+  Kerja: S + Verb I + O,
+  Sifat: S + to be (is/am/are) + noun/adjective/adverb.
+
+ Present continous tense
+  Kerja: S + is/am/are + V.I + ing + object,
+  Sifat: S + is/am/are + noun/adjective/adverb.
+
+ Present perfect tense
+  Kerja: S + have/has + Verb III + O,
+  Sifat: S + have/has + been + noun/adjective/adverb.
+
+ Present perfect continous tense
+  Kerja: S + has/have + been + Verb I + ing + since/for,
+  Sifat: S + has/have + been + Verb I + ing + noun/adjective/adverb + since/for.
+
+2. PAST
+
+ Simple past tense
+  Kerja: S + Verb II + O,
+  Sifat: S + was/were + noun/adjective/adverb.
+
+ Past continous tense
+  Kerja: S + was/were + Verb I + ing + O,
+  Sifat: S + was/were + noun/adjective/adverb.
+
+ Past perfect tense
+  Kerja: S + + had + Verb III + O,
+  Sifat: S + had + been + noun/adjective/adverb.
+
+ Past perfect continous tense
+  Kerja: S + had + been + Verb I + ing + since/for,
+  Sifat: S + had + been + Verb I + ing + noun/adjective/adverb + since/for.
+ 
+
+3. FUTURE
+ 
+ Simple future tense
+  Kerja: S + will + Verb I + O,
+  Sifat: S + will + be + noun/adjective/adverb.
+
+ Future continous tense
+  Kerja: S + will + be + verb I + ing + object
+  Sifat: S + will + be + + noun/adjective/adverb.
+
+ Past future tense
+  Kerja: S + would + Verb I + O,
+  Sifat: S + would + be + noun/adjective/adverb.
+
+ Future perfect continous tense
+  Kerja: S + will + have + been + Verb I + ing + since/for,
+  Sifat: S + will + have + been + Verb I + ing + noun/adjective/adverb + since/for.
+
+4. PAST X FUTURE
+
+ Simple past future tense
+  Kerja: S + would/should + Verb I + O,
+  Sifat: S + would/should + to be (is/am/are) + noun/adjective/adverb.
+
+ Past future continous tense
+  Kerja: S + would/should + is/am/are + V.I + ing + object,
+  Sifat: S + would/should + is/am/are + noun/adjective/adverb.
+
+ Past future perfect tense
+  Kerja: S + would/should + have + Verb III + O,
+  Sifat: S + would/should + have + been + noun/adjective/adverb.
+
+ Past future perfect continous tense
+  Kerja: S + would/should + has + been + Verb I + ing + since/for,
+  Sifat: S + would/should + has + been + Verb I + ing + noun/adjective/adverb + since/for.
